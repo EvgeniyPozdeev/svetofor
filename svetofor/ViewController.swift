0304c814e826603e77bd809a1230d1f863cff162
gkelektro <gkelektro1@gmail.com>
2023-01-23 18:37:25 +0300

    Initial Commit

---
+++ svetofor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  svetofor
+//
+//  Created by Evgeniy Pozdeev on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
